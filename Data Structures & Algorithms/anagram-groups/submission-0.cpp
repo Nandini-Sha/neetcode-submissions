ab0713bdf1fe6b23866415d24caeca74c931c2dc
Nandini Bhardwaj <pnnandu808@gmail.com>
2026-06-11 13:58:37 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());  
+            groups[key].push_back(s);
+
+        }
+
+        vector<vector<string>> answer;
+        for (auto& it : groups) {
+            answer.push_back(it.second);
+        }
+
+        return answer;
+    }
+};
+
